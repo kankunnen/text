665d09e50570b4d391d91a4464353df6d4034e49
Istvan Feli <ifeli@ifeli02.szintezis-net.local>
2015-09-22 21:43:39 +0200

    Commits added

--- src/main/java/com/infiniteskills/maven/ApplicationOne.java
+++ src/main/java/com/infiniteskills/maven/ApplicationOne.java
@@ -3,6 +3,7 @@ package com.infiniteskills.maven;
 public class ApplicationOne {
 	public static void main(String[] args){
 	
+		//CR1 the missing comment is added;
 		System.out.println("Hello world, hello maven");
 	}
 
