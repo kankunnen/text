25a40df5bec1d39f867ea85bb15526e5ba43684d
Istvan Feli <ifeli@ifeli02.szintezis-net.local>
2015-09-22 22:23:06 +0200

    Bug comment add

--- src/main/java/com/infiniteskills/maven/ApplicationOne.java
+++ src/main/java/com/infiniteskills/maven/ApplicationOne.java
@@ -5,6 +5,7 @@ public class ApplicationOne {
 	
 		//CR1 the missing comment is added;
 		//Additional comment is added;
+		//Comment added in the master to fix issue;
 		System.out.println("Hello world, hello maven");
 	}
 
